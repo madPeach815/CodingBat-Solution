d5cbe461c9c4444b2eae373aca43fd3aab5579af
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-03-27 17:10:52 -0400

    Create more14.java

---
+++ Array-2/more14.java
@@ -0,0 +1,19 @@
+public boolean more14(int[] nums) {
+  //Given an array of ints, 
+  //return true if the number of 1's is greater than the number of 4's
+  int count1 = 0;
+  int count4 = 0;
+  
+  for(int i = 0; i < nums.length; i++){
+    if(nums[i] == 1){
+      count1++;
+    }
+    if(nums[i] == 4){
+      count4++;
+    }
+  }
+  if(count1 > count4){
+    return true;
+  }
+  return false;
+}
