1a304feb6674a1a6dd1941b288d8c277da43879e
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-03-27 17:05:12 -0400

    Create sum28.java

---
+++ Array-2/sum28.java
@@ -0,0 +1,15 @@
+//Given an array of ints, 
+//return true if the sum of all the 2's in the array is exactly 8.
+
+public boolean sum28(int[] nums) {
+  int sum = 0;
+  for(int i = 0; i < nums.length; i++){
+    if(nums[i] == 2){
+      sum += 2;
+    }
+  }
+  if(sum == 8){
+    return true;
+  }
+  return false;
+}
