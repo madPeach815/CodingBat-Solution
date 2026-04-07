c454ae316118dc7779d9cb011b3e5eac48c8da1e
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-04-07 14:38:20 -0400

    Create ShiftLeft.java

---
+++ Array-2/ShiftLeft.java
@@ -0,0 +1,19 @@
+public int[] shiftLeft(int[] nums) {
+  //Return an array that is "left shifted" by one -- 
+  //so {6, 2, 5, 3} returns {2, 5, 3, 6}. You may modify and return the given array, 
+  //or return a new array.
+  
+  int[] temp = new int[nums.length];
+  
+  if(nums.length > 1){
+    for(int i = 0; i < nums.length-1; i++){
+      temp[i] = nums[i+1];
+      temp[nums.length-1] = nums[0];
+    } 
+  } else {
+    return nums;
+  }
+  nums = temp;
+  
+  return nums;
+}
