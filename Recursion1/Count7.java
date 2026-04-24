9e0d5e4bac51df119f5ee4f13f166ab88a0b031e
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-04-24 11:59:28 -0400

    Create Count7.java

---
+++ Recursion1/Count7.java
@@ -0,0 +1,19 @@
+/*
+Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+*/
+
+public int count7(int n) {
+  //n is 0 and positive integer
+  //count how many times 7 occurr 
+  //% 10 yields the rightmost digit
+  // /10 removes the rightmost digit
+  int count = 0;
+  
+  if(n == 0){ 
+    return 0;
+  }
+  if(n % 10 == 7){
+    count = 1;
+  }
+  return count + count7(n/10);
+}
