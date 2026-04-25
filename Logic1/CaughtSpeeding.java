c461fec3a3b324ceed648e171d6e881349f29902
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-04-25 12:28:55 -0400

    Create CaughtSpeeding.java

---
+++ Logic1/CaughtSpeeding.java
@@ -0,0 +1,21 @@
+/*
+You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.*/
+
+public int caughtSpeeding(int speed, boolean isBirthday) {
+  //return 0 = no ticket, 1 = small ticket, 2 = big ticket
+  //if(speed >= 81) = 2
+  //(61 <= speed <= 80) = 1
+  //if(speed <= 60) = 0
+  //isBirthday speed can be higher all cases
+  if(isBirthday){
+    speed = speed - 5;
+  }
+
+    if(speed <= 60){
+      return 0;
+    }else if (speed >= 61 && speed <= 81){
+      return 1;
+    }else {
+      return 2;
+    }
+  }
