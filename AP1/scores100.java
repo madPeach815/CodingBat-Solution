695395fed9229c68dc1f5cc8290dd0655908608d
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-04-26 23:21:15 -0400

    Create scores100.java

---
+++ AP1/scores100.java
@@ -0,0 +1,13 @@
+/* Given an array of scores, return true if there are scores of 100 next to each other in the array. The array length will be at least 2. */
+
+public boolean scores100(int[] scores) {
+//if there are score of 100 next to each other... true
+//Let's find true
+  
+  for(int i = 0; i < scores.length - 1; i++){
+    if(scores[i] == 100 && scores[i+1] == 100){
+        return true;
+      }
+    }
+  return false;
+}
