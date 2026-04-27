2fe5c00b9bc754da3ce178e147ab77203bde1160
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-04-26 23:40:39 -0400

    Create noX.java

---
+++ Recursion1/noX.java
@@ -0,0 +1,13 @@
+public String noX(String str) {
+
+/* Given a string, compute recursively a new string where all the 'x' chars have
+been removed.*/
+
+  if(str.length() == 0){
+    return str;
+  }
+  if(str.charAt(0) == 'x'){
+    return noX(str.substring(1));
+  }
+  return str.charAt(0) + noX(str.substring(1));
+}
