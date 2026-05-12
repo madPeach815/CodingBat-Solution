c3c084bfea543ff6beb3e8e91c1a3e0c6d8f75ec
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-05-12 16:45:58 -0400

    Create strDist.java

---
+++ Recursion1/strDist.java
@@ -0,0 +1,19 @@
+/*
+Given a string and a non-empty substring sub, compute recursively the largest substring which starts and ends with sub and return its length.
+*/
+
+public int strDist(String str, String sub) {
+  
+if(str.length() < sub.length()){
+  return 0;
+}  
+  
+if(!str.startsWith(sub)){
+  return strDist(str.substring(1), sub);
+}
+if(!str.endsWith(sub)){
+  return strDist(str.substring(0, str.length()-1), sub);
+}
+
+return str.length();
+}
