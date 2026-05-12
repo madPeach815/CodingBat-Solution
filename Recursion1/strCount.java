9f21784461ba708918ff916e251401a71de95f40
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-05-12 15:55:36 -0400

    Create strCount.java

---
+++ Recursion1/strCount.java
@@ -0,0 +1,16 @@
+/*
+Given a string and a non-empty substring sub, compute recursively the number of times that sub appears in the string, without the sub strings overlapping.
+*/
+
+public int strCount(String str, String sub) {
+  //no overlapping
+  
+  if(str.length() < sub.length()){
+    return 0;
+  }
+  
+  if(str.startsWith(sub)){
+    return 1 + strCount(str.substring(sub.length()), sub);
+  }
+  return strCount(str.substring(1), sub);
+}
