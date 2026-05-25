2c43fdaed97a18245552c5a26a5597b4df1b4af1
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-05-24 23:33:33 -0400

    Create fizzString.java

---
+++ Logic1/fizzString.java
@@ -0,0 +1,39 @@
+/*
+Given a string str, if the string starts with "f" return "Fizz". If the string ends with "b" return "Buzz". If both the "f" and "b" conditions are true, return "FizzBuzz". In all other cases, return the string unchanged. 
+*/
+
+public String fizzString(String str) {
+  if(str.charAt(0) == 'f' && str.charAt(str.length()-1) == 'b'){
+    return "FizzBuzz";
+  }
+  if(str.charAt(0) == 'f'){
+    return "Fizz";
+  }
+  if(str.charAt(str.length()-1) == 'b'){
+    return "Buzz";
+  }
+  
+  return str;
+}
+
+//Using startsWith() and endsWith() instead of charAt()
+
+public String fizzString(String str) {
+
+  boolean startsWithF = str.startsWith("f");
+  boolean endsWithB = str.endsWith("b");
+  
+  if(startsWithF && endsWithB){
+    return "FizzBuzz";
+  }
+  
+  if(startsWithF){
+    return "Fizz";
+  }
+  
+  if(endsWithB){
+    return "Buzz";
+  }
+
+  return str;
+}
