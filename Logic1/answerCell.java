bf18c5eb640225b0123b0eab95fa547b3938fed6
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-05-24 23:17:43 -0400

    Create answerCell.java

---
+++ Logic1/answerCell.java
@@ -0,0 +1,18 @@
+/*
+Your cell phone rings. Return true if you should answer it. Normally you answer, except in the morning you only answer if it is your mom calling. In all cases, if you are asleep, you do not answer.
+*/
+
+public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+  //first isAsleep return false
+  //second isMorning and !isMom return false;
+  //return true;
+  
+  if(isAsleep){
+    return false;
+  }
+  if(isMorning && !isMom){
+    return false;
+  }
+  
+  return true;
+}
