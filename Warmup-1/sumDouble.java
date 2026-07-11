497540d286ccda86a80d9a12442413a6f193e561
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-07-11 16:50:51 -0400

    Create sumDouble.java

---
+++ Warmup-1/sumDouble.java
@@ -0,0 +1,9 @@
+public int sumDouble(int a, int b) {
+  /*Given two int values, return their sum. Unless the two values are the same, 
+  then return double their sum */
+  
+  if(a == b){
+    return 2 * (a + b);
+  }
+  return a + b;
+}
