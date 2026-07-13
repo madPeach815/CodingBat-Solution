f3abf957330345b2092f008694249fc8392a6c01
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-07-13 10:55:32 -0400

    Create or35.java

---
+++ Warmup-1/or35.java
@@ -0,0 +1,6 @@
+public boolean or35(int n) {
+  //Return true if the given non-negative number is a multiple of 3 or a multiple 
+  //of 5. Use the % "mod" poerator. 
+  
+  return n % 3 == 0 || n % 5 == 0;
+}
