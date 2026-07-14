b29d8851ae76c46a8e6c7b32eb15914826809ddf
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-07-13 23:12:37 -0400

    Create in3050.java

---
+++ Warmup-1/in3050.java
@@ -0,0 +1,8 @@
+public boolean in3050(int a, int b) {
+  //Given 2 int values, return true if they are both in the range 30..40 inclusive, 
+  //or they are both in the range 40..50 inclusive. 
+  
+return ((a >= 30 && a <= 40) && (b >= 30 && b <= 40)) || 
+        ((a >= 40 && a <= 50) && (b >= 40 && b <= 50));
+
+}
