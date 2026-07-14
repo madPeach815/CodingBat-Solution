11ee6b2364e4a0a1c87b68e00822c8244c5249d7
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-07-13 23:53:05 -0400

    Create everyNth.java

---
+++ Warmup-1/everyNth.java
@@ -0,0 +1,14 @@
+public String everyNth(String str, int n) {
+  //Given a non-empty string and an int N, return the string made starting with 
+  //char 0, and then every Nth char of the string.
+  //So if N is 3, use char 0, 3, 6 ... and so on.
+  //N is 1 or more. 
+  
+  String result = "";
+  
+  for(int i = 0; i < str.length(); i+= n){
+    result += str.charAt(i);
+  }
+  
+  return result;
+}
