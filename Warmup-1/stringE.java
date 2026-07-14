85796f1e3351f34dc279535784b6e65757e3dd12
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-07-13 23:38:18 -0400

    Create stringE.java

---
+++ Warmup-1/stringE.java
@@ -0,0 +1,11 @@
+public boolean stringE(String str) {
+  //Return true if the given string contains between 1 and 3 'e' chars. 
+  int count = 0;
+  
+  for(int i = 0; i < str.length(); i++){
+    if(str.charAt(i) == 'e'){
+      count++;
+    }
+  }
+  return count >= 1 && count <= 3;
+}
