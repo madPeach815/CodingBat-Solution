d08c21b1552c8941d0b9f59494c57c836d9d8975
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-07-15 00:21:01 -0400

    Create arrayFront9.java

---
+++ Warmup-2/arrayFront9.java
@@ -0,0 +1,14 @@
+public boolean arrayFront9(int[] nums) {
+  //Given an array of ints, return true if one of the first 4 elements in the 
+  //array is a 9. The array length may be less than 4. 
+  
+  int end = Math.min(nums.length, 4);
+ 
+    for(int i = 0; i < end; i++){
+      if(nums[i] == 9){
+        return true;
+      }
+    }
+  
+  return false;
+}
