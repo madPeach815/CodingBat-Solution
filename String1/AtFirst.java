74914624ff8c7c9c8c1062809c067538d2fa208e
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-07-21 15:42:51 -0400

    Create AtFirst.java

---
+++ String1/AtFirst.java
@@ -0,0 +1,14 @@
+public String atFirst(String str) {
+  //Given a stirng, return a string length 2 made of its first 2 chars. 
+  //If the string length is less than 2, use '@' for the missing chars. 
+  
+  if(str.length() < 1) {
+    return "@@";
+  }
+  if(str.length() == 1) {
+    return str + "@";
+  }
+ 
+  return str.substring(0, 2);
+  
+}
