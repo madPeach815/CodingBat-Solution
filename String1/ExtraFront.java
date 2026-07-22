eee86e9f3b77ea0f29a47b57d2cb315e97172378
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-07-22 16:23:32 -0400

    Create ExtraFront.java

---
+++ String1/ExtraFront.java
@@ -0,0 +1,11 @@
+public String extraFront(String str) {
+  //Given a string, return a new string made of 3 copies of the first 2 chars of
+  //the original string. The string may be any length. If there are fewer than 
+  //2 chars, use whatever is there. 
+  
+  if(str.length() < 2){
+    return str + str + str;
+  }
+  
+  return str.substring(0, 2) + str.substring(0, 2) + str.substring(0, 2);
+}
