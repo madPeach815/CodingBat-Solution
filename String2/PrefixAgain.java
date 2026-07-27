58b57d9a11391b8f83d4a5ca59bd77aa75aa3fec
madPeach <271130229+madPeach815@users.noreply.github.com>
2026-07-27 10:23:42 -0400

    Create PrefixAgain.java

---
+++ String2/PrefixAgain.java
@@ -0,0 +1,27 @@
+public boolean prefixAgain(String str, int n) {
+  //Given a string, consider the prefix string made of the first N chars of the 
+  //string. Does that prefix string appear somewhere else in the string?
+  //Assume that the string is not empty and that N is in the range 1.. str.length()
+  
+  String prefix = str.substring(0, n);
+
+  for(int i = n; i < str.length(); i++){
+    if(str.substring(i).startsWith(prefix)){
+      return true;
+    }
+  }
+  return false;
+}
+
+
+// or
+
+public boolean prefixAgain(String str, int n) {
+  //Given a string, consider the prefix string made of the first N chars of the 
+  //string. Does that prefix string appear somewhere else in the string?
+  //Assume that the string is not empty and that N is in the range 1.. str.length()
+  
+  String prefix = str.substring(0, n);
+
+  return str.indexOf(prefix, n) != -1;
+}
