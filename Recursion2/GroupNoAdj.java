39abc2a4d8c0f9102936582189a2844fceb7de6a
Jemma Lee <271130229+madPeach815@users.noreply.github.com>
2026-08-04 23:10:26 -0400

    Create GroupNoAdj.java

---
+++ Recursion2/GroupNoAdj.java
@@ -0,0 +1,17 @@
+//Given an array of ints, is it possible to choose a group of some of the ints, 
+//such that the group sums to the given target with this additional constraint:
+//If a value in the array is chosen to be in the group, the value immediately 
+//following it in the array must not be chosen. (No loops needed.)
+
+public boolean groupNoAdj(int start, int[] nums, int target) {
+  //one select next not select.
+  //some of sum must be same as target
+  
+  //base case
+  if(start >= nums.length){
+    return target == 0; 
+  }
+  
+  return groupNoAdj(start + 2, nums, target - nums[start])
+        || groupNoAdj(start + 1, nums, target);
+}
